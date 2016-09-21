1a5965810d084b530ffe7b6fa5c3cf271e33977b
AcostaDemian <acostademianariel@gmail.com>
2016-09-21 13:33:08 -0300

    Flecha que muestra la direccion agregada

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -157,11 +157,53 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('movimientoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('movimientoCtrl', ['$scope', '$stateParams','$cordovaDeviceMotion', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $cordovaFile) {
+function ($scope, $stateParams,$cordovaDeviceMotion) {
 
+  $scope.x;
+  $scope.y;
+  $scope.z;
+  $scope.timestamp;
+
+  $scope.imagen= "img/pokeball.png";
+
+  var options = { frequency: 100 };
+
+  document.addEventListener("deviceready", function () {
+
+    var watch = $cordovaDeviceMotion.watchAcceleration(options);
+    watch.then(
+      null,
+      function(error) {
+      // An error occurred
+      },
+      function(result) {
+
+        if(result.x > 8)
+        {
+          $scope.imagen= "img/izquierda.png";
+        }
+        if(result.x < -8)
+        {
+          $scope.imagen= "img/derecha.png";
+        }
+
+        if(result.y > 8)
+        {
+          $scope.imagen= "img/abajo.png";
+        }
+        if(result.y < -8)
+        {
+          $scope.imagen= "img/arriba.png";
+        }
+        if(result.x ==0 && result.y ==0)
+          $scope.imagen= "img/pokeball.png";
+
+    });
+
+  }, false);
 
 }])
    
